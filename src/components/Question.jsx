81b46298eb64e1152f8a4ef4a3a70ea05482dad7
sakib071 <c193055@ugrad.iiuc.ac.bd>
2024-01-11 04:24:24 +0600

    update

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -5,10 +5,12 @@ const Question = ({ question, onNextQuestion, onAnswer }) => {
     const [selectedOption, setSelectedOption] = useState(null);
 
     const handleOptionChange = (event) => {
+        console.log("Selected Option:", event.target.value);
         setSelectedOption(event.target.value);
     };
 
-    const handleSubmit = () => {
+    const handleSubmit = (event) => {
+        event.preventDefault();
         const isCorrect = selectedOption === question.correctAnswer;
         onAnswer(isCorrect);
         onNextQuestion();
@@ -20,43 +22,48 @@ const Question = ({ question, onNextQuestion, onAnswer }) => {
             .then(data => setQues(data))
     }, [])
 
+    // useEffect(() => {
+    //     // Cleanup function to reset selectedOption when component unmounts
+    //     return () => setSelectedOption(null);
+    // }, []);
+
     return (
         <div className="h-screen flex flex-col justify-center items-center">
-            <div key={question.id} className="card lg:w-96 w-80 bg-base-100 rounded-md shadow-xl">
-                <div className="card-body">
-                    <div className="flex w-full justify-end">
-                        <h3 className="text-sm font-semibold">{question.id}/{ques.length}</h3>
-                    </div>
-                    <h2 className="card-title mb-5 h-20">{question.question}</h2>
-                    <ul className="flex flex-col gap-3">
-                        {question.options.map((option, index) => (
-                            <li key={index} className="flex items-center gap-2">
-                                <input
-                                    type="radio"
-                                    id={`option-${index}`}
-                                    name={`question-${question.id}`}
-                                    value={option}
-                                    className="radio radio-sm"
-                                    onChange={handleOptionChange}
-                                    checked={selectedOption === option}
-                                />
-                                <label htmlFor={`option-${index}`}>{option}</label>
-                            </li>
-                        ))}
-                    </ul>
-                    <div className="card-actions justify-end">
-                        <button
-                            className="btn btn-sm bg-yellow-300 hover:bg-gray-800 hover:text-white"
-                            onClick={handleSubmit}
-                        >
-                            Submit
-                        </button>
+            <form onSubmit={handleSubmit}>
+                <div key={question.id} className="card lg:w-96 w-80 bg-base-100 rounded-md shadow-xl">
+                    <div className="card-body">
+                        <div className="flex w-full justify-end">
+                            <h3 className="text-sm font-semibold">{question.id}/{ques.length}</h3>
+                        </div>
+                        <h2 className="card-title mb-5 h-20">{question.question}</h2>
+                        <ul className="flex flex-col gap-3">
+                            {question.options.map((option, index) => (
+                                <li key={index} className="flex items-center gap-2">
+                                    <input
+                                        type="radio"
+                                        id={`option-${index}`}
+                                        name={`question-${question.id}`}
+                                        value={option}
+                                        className="radio radio-sm"
+                                        onChange={handleOptionChange}
+                                        checked={selectedOption === option}
+                                        required
+                                    />
+                                    <label htmlFor={`option-${index}`}>{option}</label>
+                                </li>
+                            ))}
+                        </ul>
+                        <div className="card-actions justify-end">
+                            <button
+                                type="submit"
+                                className="btn btn-sm bg-yellow-300 hover:bg-gray-800 hover:text-white"
+                            >Submit</button>
+                        </div>
                     </div>
                 </div>
-            </div>
+            </form>
         </div>
     );
 };
-
 
 export default Question;
